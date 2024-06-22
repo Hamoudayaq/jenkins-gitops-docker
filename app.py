961201b306b8c796e2be419911f265d13753b991
Hamoudayaq <134491095+Hamoudayaq@users.noreply.github.com>
2024-06-22 16:35:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '3ezdineee yerfes'
+    return 'Touta mezyenaaaaa ❤️'
     
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
